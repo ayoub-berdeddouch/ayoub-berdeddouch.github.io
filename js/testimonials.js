e92aa94bbd747c03aad2834de5799a2436b4d0e7
Ayoub BERDEDDOUCH <ayoub.berd@gmail.com>
2020-04-28 23:20:11 +0000

    add testimonial.js

---
+++ js/testimonials.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $("#testimonial-slider").owlCarousel({
+        items:1,
+        itemsDesktop:[1000,1],
+        itemsDesktopSmall:[979,1],
+        itemsTablet:[768,1],
+        pagination:true,
+        navigation:false,
+        navigationText:["",""],
+        slideSpeed:1000,
+        singleItem:true,
+        autoPlay:true
+    });
+});
